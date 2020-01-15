9cb81b0605649c9733adf63dabe3ad22121c1a00
Attila Juhos <juhosattila6@gmail.com>
2020-01-15 08:44:58 +0100

    Prediction trying out.

--- modules/NeuralNetworks.py
+++ modules/NeuralNetworks.py
@@ -7,6 +7,7 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.models import load_model
 
 from abc import ABC, abstractmethod
+import json
 
 from modules.Metrics import *
 
@@ -15,7 +16,8 @@ models_folder = 'BigDatasetsHW/Models/'
 
 def get_model_file_path(model_name):
     return os.path.join(models_folder, model_name + '.h5')
-
+def get_model_decoding_path(model_name):
+    return os.path.join(models_folder, model_name + '.json')
 
 class AbstractNeuralNetwork(ABC):
 
@@ -58,17 +60,7 @@ class InceptionNeuralNetwork(AbstractNeuralNetwork):
         self.metrics = ['categorical_accuracy']
         self.__compile(optimizer=Adam(lr=0.25))
 
-    def save(self, model_name):
-        self.model.save(get_model_file_path(model_name))
-
-    def load(self, model_name):
-        self.__load_model(get_model_file_path(model_name))
-
-    def predict(self, x):
-        return self.model.predict(x)
-
-    def summary(self):
-        print(self.model.summary())
+        self.class_indices = None
 
     def __compile(self, optimizer):
         self.model.compile(optimizer=optimizer,
@@ -85,6 +77,26 @@ class InceptionNeuralNetwork(AbstractNeuralNetwork):
 
         self.model = load_model(model_name, custom_objects={'metrics': self.metrics})
 
+    def save(self, model_name):
+        self.model.save(get_model_file_path(model_name))
+
+        class_file_name = get_model_decoding_path(model_name)
+        with open(class_file_name, 'w') as f:
+            json.dump(self.class_indices, f)
+
+    def load(self, model_name):
+        self.__load_model(get_model_file_path(model_name))
+
+        class_file_name = get_model_decoding_path(model_name)
+        with open(class_file_name, 'r') as f:
+            self.class_indices = json.load(f)
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+    def summary(self):
+        print(self.model.summary())
+
     def fit_generator(self, train_generator_iterator, validation_generator_iterator):
         # Freeze the base model layers and train only the newly added layers.
         for layer in self.base_model.layers:
@@ -98,7 +110,7 @@ class InceptionNeuralNetwork(AbstractNeuralNetwork):
 
         # After all, train the upper layers of the  network.
         self.model.fit_generator(train_generator_iterator,
-                                 epochs=5,
+                                 epochs=1,
                                  validation_data=validation_generator_iterator,
                                  shuffle=True,
                                  callbacks=[checkpointer, earlystopping])
@@ -115,12 +127,15 @@ class InceptionNeuralNetwork(AbstractNeuralNetwork):
         self.__compile(optimizer=SGD(lr=0.0001, momentum=0.9, nesterov=True))
 
         self.model.fit_generator(train_generator_iterator,
-                                 epochs=5,
+                                 epochs=1,
                                  validation_data=validation_generator_iterator,
                                  shuffle=True,
                                  callbacks=[checkpointer, earlystopping])
 
         self.__load_model()
+
+        # Retrieve class indices.
+        self.class_indices = train_generator_iterator.class_indices
 
     def evaluate_generator(self, test_generator_iterator):
         # Evaluate the model on the test set.
@@ -130,6 +145,16 @@ class InceptionNeuralNetwork(AbstractNeuralNetwork):
     def set_metrics(self, *args):
         self.metrics.clear()
         self.metrics.extend(args)
+
+    def decode_predictions(self, predictions, threshold=0.5):
+        class_indices = self.class_indices
+        result = []
+        single_prediction = predictions[0]  # TODO extend the function to handle multiple predictions
+        for label, prediction in zip(class_indices, single_prediction):
+            if prediction >= threshold:
+                result.append((label, prediction))
+        result = sorted(result, key=lambda tup: tup[1], reverse=True)
+        return result
 
 
 class InceptionNeuralNetwork1(InceptionNeuralNetwork):
@@ -144,6 +169,7 @@ class InceptionNeuralNetwork1(InceptionNeuralNetwork):
         x = Dense(256, activation='relu')(x)
         x = Dense(256, activation='relu')(x)
         return x
+
 
 class InceptionNeuralNetwork2(InceptionNeuralNetwork):
     def __init__(self, output_target_size):
